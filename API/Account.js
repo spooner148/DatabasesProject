068dbef071e5da281642c3b1d2fdd239e273197d
samhyatt <47097203+samhyatt@users.noreply.github.com>
2019-04-08 13:59:48 -0400

    Update Account.js

--- API/Account.js
+++ API/Account.js
@@ -21,6 +21,152 @@ function setUpdateId(val)
     localStorage.setItem("Updateid",val);
 }
 
+function searchTakeSurveys() 
+{
+    var search = document.getElementById("inlineFormInputName").value;
+    
+    if (localStorage.hasOwnProperty("Userid"))
+    {
+        var xhr= new XMLHttpRequest();
+        xhr.open("POST","./SearchSurveys.php",false);
+        xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+        
+        var Userid = localStorage.getItem("Userid");
+        
+        var jsonPayload = '{"Search" : "' + search + '", "Userid" : "' + Userid + '"}';
+        
+        try
+        {
+            xhr.onreadystatechange = function()
+            {
+                if (this.readyState == 4 && this.status == 200)
+                {
+                    hideOrShow( "contactList", true );
+                    var str = xhr.responseText;
+                    
+                   // else
+                   // {
+                        var jsonObject = JSON.parse( xhr.responseText );
+                        var table = document.getElementById("TakeList");
+                        table.deleteTHead();
+                        if(str.includes("No Records Found"))
+                        {
+                            
+                            var newSurvey = table.createTHead();
+                            var newSurveyinfo = newSurvey.insertRow(0);
+                            newSurveyinfo.scope = "row";
+                            newSurveyinfo.insertCell(0).outerHTML = '<th scope="col">No matching Surveys found</th>';
+                        }
+                            for (var i = 0; i < jsonObject.results.length; i++)
+                            {
+                                //var opt = document.createElement("option");
+                                var jsonObjectTwo = jsonObject.results[i];
+                                var error = jsonObjectTwo.error;
+                                if (error == "")
+                                {
+                                    var SurveyName = jsonObjectTwo.SurveyText +" "+ jsonObjectTwo.SurveyLastName;
+                                    var newSurvey = table.createTHead(jsonObjectTwo.Userid);
+                                    var newSurveyinfo = newSurvey.insertRow(0);
+                                    newSurveyinfo.scope = "row";
+                                    newSurveyinfo.value = "1";
+                                    newSurveyinfo.insertCell(0).outerHTML = '<th scope="col">'+(jsonObject.results.length - i)+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
+                                    newSurveyinfo.insertCell(1).outerHTML = '<th scope="col">'+jsonObjectTwo.SurveyText+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
+                                    newSurveyinfo.insertCell(2).outerHTML = '<th scope="col"><button type="button" value="'+jsonObjectTwo.Surveyid+'" class="btn btn-primary btn" onclick="deleteThis(this, this.value)">Dismiss</button></th>';
+                                    
+                                }
+                            }
+                    //}
+                    
+                    
+                }
+            };
+            xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+            document.getElementById("SurveySearchResult").innerHTML = err.message;
+            alert("BIG ERROR BRO");
+        }
+    }
+    else 
+    {
+        window.location.assign("splash.html");
+    }
+}
+
+function searchViewSurveys() 
+{
+    var search = document.getElementById("inlineFormInputName").value;
+    
+    if (localStorage.hasOwnProperty("Userid"))
+    {
+        var xhr= new XMLHttpRequest();
+        xhr.open("POST","./SearchSurveys.php",false);
+        xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+        
+        var Userid = localStorage.getItem("Userid");
+        
+        var jsonPayload = '{"Search" : "' + search + '", "Userid" : "' + Userid + '"}';
+        
+        try
+        {
+            xhr.onreadystatechange = function()
+            {
+                if (this.readyState == 4 && this.status == 200)
+                {
+                    hideOrShow( "contactList", true );
+                    var str = xhr.responseText;
+                    
+                   // else
+                   // {
+                        var jsonObject = JSON.parse( xhr.responseText );
+                        var table = document.getElementById("ViewList");
+                        table.deleteTHead();
+                        if(str.includes("No Records Found"))
+                        {
+                            
+                            var newSurvey = table.createTHead();
+                            var newSurveyinfo = newSurvey.insertRow(0);
+                            newSurveyinfo.scope = "row";
+                            newSurveyinfo.insertCell(0).outerHTML = '<th scope="col">No matching Surveys found</th>';
+                        }
+                            for (var i = 0; i < jsonObject.results.length; i++)
+                            {
+                                //var opt = document.createElement("option");
+                                var jsonObjectTwo = jsonObject.results[i];
+                                var error = jsonObjectTwo.error;
+                                if (error == "")
+                                {
+                                    var SurveyName = jsonObjectTwo.SurveyText +" "+ jsonObjectTwo.SurveyLastName;
+                                    var newSurvey = table.createTHead(jsonObjectTwo.Userid);
+                                    var newSurveyinfo = newSurvey.insertRow(0);
+                                    newSurveyinfo.scope = "row";
+                                    newSurveyinfo.value = "1";
+                                    newSurveyinfo.insertCell(0).outerHTML = '<th scope="col">'+(jsonObject.results.length - i)+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
+                                    newSurveyinfo.insertCell(1).outerHTML = '<th scope="col">'+jsonObjectTwo.SurveyText+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>";
+                                    newSurveyinfo.insertCell(2).outerHTML = '<th scope="col"><button type="button" value="'+jsonObjectTwo.Surveyid+'" class="btn btn-primary btn" onclick="deleteThis(this, this.value)">Dismiss</button></th>';
+                                    
+                                }
+                            }
+                    //}
+                    
+                    
+                }
+            };
+            xhr.send(jsonPayload);
+        }
+        catch(err)
+        {
+            document.getElementById("SurveySearchResult").innerHTML = err.message;
+            alert("BIG ERROR BRO");
+        }
+    }
+    else 
+    {
+        window.location.assign("splash.html");
+    }
+}
+
 function searchQuestions() 
 {
     var search = document.getElementById("inlineFormInputName").value;
@@ -93,7 +239,7 @@ function searchQuestions()
     }
     else 
     {
-        window.location.assign("index.html");
+        window.location.assign("splash.html");
     }
 }
 
