7f290d16f984788fb31d2d2cab334d6efacb0d5a
ImmortanRaptus <49250280+ImmortanRaptus@users.noreply.github.com>
2019-04-14 02:59:01 -0400

    Add files via upload

--- routes/answerSurvey.js
+++ routes/answerSurvey.js
@@ -44,6 +44,14 @@ var sql = mysql.createPool({
 router.get('/', function(req,res,next){
   var sid = req.query.sid;
   var email = req.cookies.user;
+  sql.query('SELECT qid FROM question WHERE sid = ?',[sid], function (error, results, fields) {
+    if (error) throw error;});
+  res.cookie('user', email).redirect('-------------------------------')
+});
+
+router.get('/continue', function(req,res,next){
+  var sid = req.query.sid;
+  var email = req.cookies.user;
   sql.query('',[], function (error, results, fields) {
     if (error) throw error;});
 });
@@ -56,12 +64,21 @@ router.post('/answerQuestionType2', function(req,res,next){
   res.render('answerSurveyt2.php', {root: './public/'  });
 });
 
+router.post('/question', function(req,res,next){
+  var qid = req.query.qid;
+  sql.query('SELECT question FROM questions WHERE qid = ?', [qid] , function (error, results, fields) {
+    if (error) throw error;
+    res.sendFile(results);
+  });
+});
+
 router.post('/submitAnswerType1', function(req,res,next){
   var email = req.cookies.user;
   var answer = req.body.answer;
   var qid = req.body.qid;
   sql.query('INSERT INTO person_answers_question (email, qid, answered) VALUES (?,?,1); INSERT INTO type1 (email, qid, answer) VALUES (?,?,?)', [email,qid,email,qid,answer] , function (error, results, fields) {
     if (error) throw error;});
+  res.redirect('/answerSurvey/continue');
 });
 
 router.post('/submitAnswerType2', function(req,res,next){
@@ -70,6 +87,7 @@ router.post('/submitAnswerType2', function(req,res,next){
   var qid = req.body.qid;
   sql.query('INSERT INTO person_answers_question (email, qid, answered) VALUES (?,?,1); INSERT INTO type2 (email, qid, answer) VALUES (?,?,?)', [email,qid,email,qid,answer] , function (error, results, fields) {
     if (error) throw error;});
+  res.redirect('/answerSurvey/continue');
 })
 module.exports = router;
 
